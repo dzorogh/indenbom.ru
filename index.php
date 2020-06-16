81589192822444d335092b03fd03332455a2aa59
dzorogh <dzorogh@gmail.com>
2020-06-16 15:06:06 +0300

    Hello!

---
+++ index.php
@@ -0,0 +1,141 @@
+﻿<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Инденбом</title>
+	<link rel="stylesheet" href="style.css?2" />
+</head>
+<body>
+	<h1>
+		Фамилия Инденбом
+	</h1>
+	<div class="tree">
+		<ul>
+			<li>
+				<span class="person">
+					<span class="firstname">
+						Аарон
+					</span>
+				</span>
+
+				<ul>
+					<li>
+						<span class="couple">
+							<a class="person" href="https://ru.wikipedia.org/wiki/%D0%98%D0%BD%D0%B4%D0%B5%D0%BD%D0%B1%D0%BE%D0%BC,_%D0%9B%D0%B5%D0%B2_%D0%90%D1%80%D0%BE%D0%BD%D0%BE%D0%B2%D0%B8%D1%87">
+								<span class="firstname">
+									Лев
+								</span>
+								<span class="years">
+									06.07.1903 — 28.04.1970
+								</span>
+							</a>
+							<span class="plus">+</span>
+							<span class="person person_sideblood">
+								<span class="firstname">
+									Хая-Сара Лейбовна (Александра Львовна)
+								</span>
+								<span class="lastname">
+									Борун
+								</span> 
+								<span class="years">
+									? — 1988
+								</span>
+							</span>
+						</span>
+
+						<ul>
+							<li>
+								<span class="couple">
+									<span class="person">
+										Владимир
+										<span class="years">
+											08.09.1924 — 05.03.1998
+										</span>
+									</span>
+									<span class="plus">+</span>
+									<span class="person person_sideblood">
+										<span class="firstname">
+											Надежда 
+										</span>
+										<span class="lastname">
+											Ананич
+										</span> 
+										<span class="years">
+											27.10.1927 — 18.06.2014
+										</span>
+									</span>
+								</span>
+								<ul>
+									<li><span class="person">Андрей</span>
+										<ul>
+											<li>
+												<a class="person" href="http://sergey.indenbom.ru/">Сергей</a>
+											</li>
+										</ul>
+									</li>
+									<li><span class="person">Михаил</span>
+										<ul>
+											<li><span class="person">Елена</span></li>
+											<li><span class="person">Максим</span></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<span class="couple">
+									<span class="person">
+										<span class="firstname">
+											Феликс 
+										</span>
+										<span class="lastname">
+											Борун
+										</span> 
+										<span class="years">
+											1927 — 1999
+										</span>
+									</span>
+									<span class="plus">+</span>
+									<span class="person person_sideblood">
+										<span class="firstname">
+											Нина
+										</span>
+										<span class="lastname">
+											Борун (Житомирская)
+										</span> 
+										<span class="years">
+											1920-е — 2010-е
+										</span>
+									</span>
+								</span>
+								<ul>
+									<li>
+										<span class="person">
+											<span class="firstname">
+												Александр
+											</span>
+											<span class="lastname">
+												Борун
+											</span> 
+										</span>
+									</li>
+									<li>
+										<span class="person">
+											<span class="firstname">
+												Виктор 
+											</span>
+											<span class="lastname">
+												Борун
+											</span> 
+										</span>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+			
+		</ul>
+	</div>
+</body>
+</html>
